53a9a39377956a28491743b4daa5052a8c64a9ae
SashaDanil <sashagodsize@gmail.com>
2025-01-22 23:47:29 +0400

    Update bot.py
    Сократили код, а также добавили две фразы

--- bot.py
+++ bot.py
@@ -1,15 +1,9 @@
-import telebot
+import telebot 
 
-API_TOKEN = '8032304693:AAH0e-7Oz3xfcOt2HNC95BRdIjDQ-j5xHSA'
-
-bot = telebot.TeleBot(API_TOKEN)
+bot = telebot.TeleBot('8032304693:AAH0e-7Oz3xfcOt2HNC95BRdIjDQ-j5xHSA')
 
 @bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, "dfgasdfasdf")
-
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
-
+def send_welcome (message):
+    bot.reply_to(message, text= "Привет,я бот для отслеживания крипты")
+    bot.reply_to(message, text= "Какую ты хочешь крипту узнать?")
 bot.polling(none_stop=True)
