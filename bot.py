78769fd4387d1578a89192bf1d5acff4cedfa138
SashaDanil <sashagodsize@gmail.com>
2025-02-01 13:53:01 +0400

    Update bot.py
    Изменил библиотеку с телебота на айограмм а также поменял расположение кнопок в начале

--- bot.py
+++ bot.py
@@ -1,67 +1,64 @@
-import telebot
-from telebot import types
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from aiogram.utils import executor
 
 API_TOKEN = '8032304693:AAH0e-7Oz3xfcOt2HNC95BRdIjDQ-j5xHSA'
 
-bot = telebot.TeleBot(API_TOKEN)
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+dp.middleware.setup(LoggingMiddleware())
 
-main_menu = types.InlineKeyboardMarkup()
-button_crypto = types.InlineKeyboardButton(text='Криптовалюта', callback_data='crypto')
-button_fiat = types.InlineKeyboardButton(text='Валюты стран', callback_data='fiat')
-button_info = types.InlineKeyboardButton(text='Информация о крипте', url='https://www.rbc.ru/crypto/')
+main_menu = InlineKeyboardMarkup()
+button_crypto = InlineKeyboardButton('Криптовалюта', callback_data='crypto')
+button_fiat = InlineKeyboardButton('Валюта стран', callback_data='fiat')
+button_news = InlineKeyboardButton('Новости о криптовалюте', url='https://www.rbc.ru/crypto/')
 main_menu.row(button_crypto, button_fiat)
-main_menu.add(button_info)
+main_menu.row(button_news)
 
-crypto_menu = types.InlineKeyboardMarkup()
-button_btc = types.InlineKeyboardButton(text='📈 Биткоин', url='https://www.rbc.ru/crypto/currency/btcusd')
-button_eth = types.InlineKeyboardButton(text='🪙 Эфириум', url='https://www.rbc.ru/crypto/currency/ethusd')
-button_xrp = types.InlineKeyboardButton(text='💸 XRP', url='https://www.rbc.ru/crypto/currency/xrpusd')
-button_trump = types.InlineKeyboardButton(text='🙍‍♂️ TRUMP', url='https://ru.investing.com/crypto/official-trump')
-button_back = types.InlineKeyboardButton(text='❌ Назад ❌', callback_data='back')
-crypto_menu.add(button_btc, button_eth, button_xrp, button_trump)
-crypto_menu.add(button_back)
+crypto_menu = InlineKeyboardMarkup()
+button_btc = InlineKeyboardButton('📈 Биткоин', url='https://www.rbc.ru/crypto/currency/btcusd')
+button_eth = InlineKeyboardButton('🪙 Эфириум', url='https://www.rbc.ru/crypto/currency/ethusd')
+button_xrp = InlineKeyboardButton('💸 XRP', url='https://www.rbc.ru/crypto/currency/xrpusd')
+button_trump = InlineKeyboardButton('🙍‍♂️ TRUMP', url='https://ru.investing.com/crypto/official-trump')
+button_back = InlineKeyboardButton('❌ Назад ❌', callback_data='back')
+crypto_menu.row(button_btc)
+crypto_menu.row(button_eth)
+crypto_menu.row(button_xrp)
+crypto_menu.row(button_trump)
+crypto_menu.row(button_back)
 
-fiat_menu = types.InlineKeyboardMarkup()
-button_dollar = types.InlineKeyboardButton(text='🇺🇲 Доллар', url='https://www.rbc.ru/quote/ticker/72413')
-button_euro = types.InlineKeyboardButton(text='🇪🇺 Евро', url='https://www.rbc.ru/quote/ticker/72383')
-button_yuan = types.InlineKeyboardButton(text='🇨🇳 Юань', url='https://www.rbc.ru/quote/ticker/72377')
-button_yen = types.InlineKeyboardButton(text='🇯🇵 Йен', url='https://ru.investing.com/currencies/jpy-rub')
-fiat_menu.add(button_dollar, button_euro, button_yuan, button_yen)
-fiat_menu.add(button_back)
+fiat_menu = InlineKeyboardMarkup()
+button_dollar = InlineKeyboardButton('🇺🇲 Доллар', url='https://www.rbc.ru/quote/ticker/72413')
+button_euro = InlineKeyboardButton('🇪🇺 Евро', url='https://www.rbc.ru/quote/ticker/72383')
+button_yuan = InlineKeyboardButton('🇨🇳 Юань', url='https://www.rbc.ru/quote/ticker/72377')
+button_yen = InlineKeyboardButton('🇯🇵 Йена', url='https://ru.investing.com/currencies/jpy-rub')
+button_back = InlineKeyboardButton('❌ Назад ❌', callback_data='back')
+fiat_menu.row(button_dollar)
+fiat_menu.row(button_euro)
+fiat_menu.row(button_yuan)
+fiat_menu.row(button_yen)
+fiat_menu.row(button_back)
 
-@bot.message_handler(commands=['start'])
-def handle_start(message):
+@dp.message_handler(commands=['start'])
+async def start_command(message: types.Message):
     image_url = 'https://etoday.kz/wp-content/uploads/2023/04/Bitcoin-780x470.jpg'
-    bot.send_photo(
+    await bot.send_photo(
         message.chat.id,
-        image_url,
-        caption='💵 Привет! Я бот для отслеживания курса криптовалют и валют. 🪙 Выберите категорию:',
+        photo=image_url,
+        caption='💵 Привет, я бот для отслеживания крипты и валюты. 🪙 Выберите категорию:',
         reply_markup=main_menu
     )
+@dp.callback_query_handler(lambda query: query.data in ['crypto', 'fiat', 'back'])
+async def inline_callback(query: types.CallbackQuery):
+    if query.data == 'crypto':
+        await query.message.edit_caption(caption='Вы выбрали "Криптовалюту".', reply_markup=crypto_menu)
+    elif query.data == 'fiat':
+        await query.message.edit_caption(caption='Вы выбрали "Валюта стран".', reply_markup=fiat_menu)
+    elif query.data == 'back':
+        await query.message.edit_caption(caption='💵 Привет, я бот для отслеживания крипты и валюты. 🪙 Выберите категорию:', reply_markup=main_menu)
+    
+    await query.answer()
 
-@bot.callback_query_handler(func=lambda call: True)
-def callback_inline(call):
-    if call.data == 'crypto':
-        bot.edit_message_caption(
-            chat_id=call.message.chat.id,
-            message_id=call.message.message_id,
-            caption='Вы выбрали "Криптовалюту".',
-            reply_markup=crypto_menu
-        )
-    elif call.data == 'fiat':
-        bot.edit_message_caption(
-            chat_id=call.message.chat.id,
-            message_id=call.message.message_id,
-            caption='Вы выбрали "Валюты стран".',
-            reply_markup=fiat_menu
-        )
-    elif call.data == 'back':
-        bot.edit_message_caption(
-            chat_id=call.message.chat.id,
-            message_id=call.message.message_id,
-            caption='💵 Привет! Я бот для отслеживания курса криптовалют и валют. 🪙 Выберите категорию:',
-            reply_markup=main_menu
-        )
-
-if __name__ == '__main__':
-    bot.polling(none_stop=True)
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
